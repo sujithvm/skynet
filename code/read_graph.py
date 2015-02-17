5cf4a111df06bd970acbcbd4a474663b8f3d81f3
sujith <sujithvm24@gmail.com>
2015-02-17 16:39:36 +0530

    add read graph script

---
+++ code/read_graph.py
@@ -0,0 +1,39 @@
+__author__ = 'Archana V Menon, Sujith V'
+
+import networkx as nx
+
+def read_graph(file_path):
+
+    """
+    This function is used to read graph from a given input file
+
+    :param file_path: path of file
+    :return: returns a graph generated from the edges read from the file
+    """
+
+    edge_list = []
+
+    # input file
+    input_file = open(file_path)
+
+    # process graph edges
+    for line in input_file.readlines() :
+        line = line.strip()
+
+        # ignore lines starting with '#'
+        if line.startswith('#') :
+            continue
+
+        edge = tuple(line.split('\t'))
+        edge_list.append(edge)
+
+    # make a new graph
+    G = nx.Graph()
+
+    # uncomment to add all egdes
+    # G.add_edges_from(edge_list)
+
+    # adding only 1st 1000 edges to graph for testing purpose only
+    G.add_edges_from(edge_list[:1000])
+
+    return G
